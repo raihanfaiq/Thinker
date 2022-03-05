0fd2ba79e74999866de3ed6398fd42a375028179
raihanfaiq <raihanfaiq20@gmail.com>
2022-03-05 18:56:21 +0700

    fix footer

--- components/_slices/Footer.tsx
+++ components/_slices/Footer.tsx
@@ -3,33 +3,35 @@ import Link from '@components/_shared/Link';
 
 const Footer = (): JSX.Element => {
 	return (
-		<div className="row">
-			<img src="/icon/tree.svg" alt="" className="absolute right-0 -bottom-[210%] z-10" />
-			<div className="container">
-				<img src="/icon/footer-logo.svg" alt="" className="relative -bottom-24 z-40" />
-			</div>
-			<div className="w-full bg-ground min-h-[180px] opacity-80" />
-			<div className="flex-cc col relative py-2 w-full bg-stone-700">
-				<div className="container flex-bc -lg:flex-cc -lg:col">
-					<div className="flex-cc gap-2">
-						<a href=''>
-							<img src="/icon/fb.svg" alt="" className="" />
-						</a>
-						<a href=''>
-							<img src="/icon/twt.svg" alt="" className="" />
-						</a>
-						<a href=''>
-							<img src="/icon/ig.svg" alt="" className="" />
-						</a>
-					</div>
-					<div className="flex-cc absolute inset-0 bottom-10 full">
-						<p className="font-medium text-white">
-						© 2022 Thinker All Right Reserved
-						</p>
+		<section className="relative">
+			<div className="row">
+				<img src="/icon/tree.svg" alt="" className="absolute right-0 xl:bottom-44  z-10" />
+				<div className="container">
+					<img src="/icon/footer-logo.svg" alt="" className="relative -bottom-24 z-40" />
+				</div>
+				<div className="w-full bg-ground min-h-[180px] opacity-80" />
+				<div className="flex-cc col relative py-2 w-full bg-stone-700">
+					<div className="container flex-bc -lg:flex-cc -lg:col">
+						<div className="flex-cc gap-2">
+							<a href=''>
+								<img src="/icon/fb.svg" alt="" className="" />
+							</a>
+							<a href=''>
+								<img src="/icon/twt.svg" alt="" className="" />
+							</a>
+							<a href=''>
+								<img src="/icon/ig.svg" alt="" className="" />
+							</a>
+						</div>
+						<div className="flex-cc absolute inset-0 bottom-10 full">
+							<p className="font-medium text-white">
+								© 2022 Thinker All Right Reserved
+							</p>
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
+		</section>
 	);
 };
 
