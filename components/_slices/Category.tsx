427cfa6079fe5b9bee9a3b80183aab6cb636d94a
ryannovianno9 <79490008+ryannovianno9@users.noreply.github.com>
2022-03-05 16:33:07 +0700

    Update Category.tsx

--- components/_slices/Category.tsx
+++ components/_slices/Category.tsx
@@ -3,6 +3,54 @@ const Category = (): JSX.Element => {
 		<section className="container mb-4 mt-16 w-full z-30">
 			<div className="bg-whitesky min-h-[450px] rounded-2xl p-10 shadow-2xl">
 				<h1 className="text-4xl font-bold text-center z-10">Produk</h1>
+				<div className="grid grid-cols-2 flex items-center justify-center">
+                    <div className="grid grid-cols-3 bg-blue-300 rounded-2xl shadow-lg my-5 mx-5 justify-">
+						<div className="flex-cc">
+							<a href="#">
+							<img src="/icon/book.svg" alt="" className="" />
+							</a>
+						</div>
+						<div className="flex-cc col gap-4 p-10 col-span-2">
+							<h1 className="text-2xl text-center font-bold">
+								Materi Pelajaran
+							</h1>
+							<h1 className="text-lg text-center z-10">
+								Cari materi pelajaran yang kamu inginkan di sini
+							</h1>
+						</div>
+                    </div>
+                    <div className="grid grid-cols-3 bg-blue-300 rounded-2xl shadow-lg my-5 mx-5">
+					<div className="flex-cc">
+							<a href="#">
+							<img src="/icon/sertif.svg" alt="" className="" />
+							</a>
+						</div>
+						<div className="flex-cc col gap-4 p-10 col-span-2">
+							<h1 className="text-2xl text-center font-bold">
+								Kumpulan Soal
+							</h1>
+							<h1 className="text-lg text-center z-10">
+								Cari soal beserta jawabannya di sini
+							</h1>
+						</div>
+                    </div>
+                    <div className="bg-blue-300 rounded-2xl shadow-lg my-5 mx-5 col-span-2 flex justify-items-center overflow-auto">
+					<div className="flex-cc ml-auto">
+							<a href="#">
+							<img src="/icon/whiteboard-teacher.svg" alt="" className="" />
+							</a>
+						</div>
+						<div className="py-10 px-20 mr-auto">
+							<h1 className="text-4xl text-center font-bold">
+								Konsultasi
+							</h1>
+							<h1 className="text-lg text-center">
+								Konsultasikan semua masalahmu di sini
+							</h1>
+						</div>
+                    </div>
+                </div>
+			</div>
 			</div>
 			<img src='/icon/cloud.png' alt="" className="w-2/5 absolute -bottom-[680px] left-0 opacity-40 -z-10" />
 			<img src='/icon/cloud.png' alt="" className="w-2/5 absolute -bottom-[660px] right-0 opacity-40 -z-10" />
