0a8559875542e55cde765d23789bc19b7396ade8
ramahartadi <ramahartadi51@gmail.com>
2022-03-13 13:39:35 +0700

    update testimony homepage

--- components/_slices/Seller/Testimony.tsx
+++ components/_slices/Seller/Testimony.tsx
@@ -1,7 +1,7 @@
 const Testimony = (): JSX.Element => {
 	return (
-		<section className="container mb-4 mt-16 w-full opacity-80 z-30">
-			<div className="bg-whitesky min-h-[450px] rounded-2xl p-10 grid divide-y-[3px] divide-black shadow-2xl">
+		<section className="container mb-4 mt-16 w-full z-30">
+			<div className="bg-whitesky min-h-[450px] rounded-2xl p-10 grid shadow-2xl opacity-90">
 				<h1 className="text-4xl font-bold text-center z-10 mb-4">
 					Yuk Dengarkan Cerita Mereka Bersama THINKER
 				</h1>
@@ -10,15 +10,16 @@ const Testimony = (): JSX.Element => {
 						<img src="/icon/standing-4.png" alt="" className="h-4/6" />
 					</div>
 					<div className="flex-cc">
-						<div className="h-64 p-5 rounded-2xl text-l border-2 border-black bg-gradient-to-bl from-sky text-justify z-10">
-							<p>&quot; THINKER adalah paket lengkap buat anak SMA dengan harga yang
-							terjangkau, kita udah bisa milih apa yang kita mau. Pokoknya THINKER
-							keren banget dan sangat bermanfaat. Terima kasih THINKER &quot;
-							<br />
-							<br />
-							Ani (8/10)-Pelajar Kelas 12 SMA Harapan
+						<div className="h-64 p-5 rounded-2xl text-l bg-gradient-to-bl from-blue-300 to-whitesky text-justify z-10 shadow-lg my-5 mx-5">
+							<p>
+								&quot; THINKER adalah paket lengkap buat anak SMA dengan harga yang
+								terjangkau, kita udah bisa milih apa yang kita mau. Pokoknya THINKER
+								keren banget dan sangat bermanfaat. Terima kasih THINKER &quot;
+								<br />
+								<br />
+								Ani (8/10)-Pelajar Kelas 12 SMA Harapan
 							</p>
-							<div className="p-2 w-20 rounded-xl border-2 border-black bg-transparent text-justify z-10 ml-64 mt-2">
+							<div className="p-2 w-20 rounded-xl bg-blue-300 text-justify z-10 ml-64 mt-2 shadow-lg">
 								<a href="#">
 									<h1 className="text-xl font-bold text-center z-10">edit</h1>
 								</a>
@@ -26,14 +27,13 @@ const Testimony = (): JSX.Element => {
 						</div>
 					</div>
 					<div className="flex-cc">
-						<div className="h-64 p-5 rounded-2xl text-l border-2 border-black bg-gradient-to-bl from-sky text-justify z-10">
+						<div className="h-64 p-5 rounded-2xl text-l  bg-gradient-to-bl from-blue-300 to-whitesky text-justify z-10 shadow-lg my-5 mx-5">
 							“Buat yang bingung mau belajar untuk ujian boleh banget nih beli materi
-							belajar dan kumpulan soal di THINKER. Bener-bener
-							ngebantu banget.”
+							belajar dan kumpulan soal di THINKER. Bener-bener ngebantu banget.”
 							<br />
 							<br />
 							Budi (9/10)-Pelajar Kelas 10 SMA Jaya
-							<div className="p-2 w-20 rounded-xl border-2 border-black bg-transparent text-justify z-10 ml-64 mt-8">
+							<div className="p-2 w-20 rounded-xl bg-blue-300 text-justify z-10 ml-64 mt-8 shadow-lg">
 								<a href="#">
 									<h1 className="text-xl font-bold text-center z-10">edit</h1>
 								</a>
