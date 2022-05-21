beedbc3829633333f62aa02f639c8cb2b57e59f3
raihanfaiq <raihanfaiq20@gmail.com>
2022-05-21 08:46:08 +0700

    faq accordion

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import MainLayout from '@components/_layouts/MainLayout';
-import { useSession } from 'next-auth/react';
-import 'tw-elements';
+import { FiPlusCircle, FiMinusCircle } from 'react-icons/fi';
+import faq from './faq.json';
 
 const About = (): JSX.Element => {
-	const { data: session, status } = useSession();
+	const [open, setOpen] = useState(0);
+	const items = faq.items;
+
 	return (
 		<div className="bg-sky">
 			<MainLayout title="Home" className="flex-sc col ">
@@ -51,244 +53,17 @@ const About = (): JSX.Element => {
 						<h1 className="text-4xl font-bold text-center z-10">
 							Frequently Asked Question
 						</h1>
-						{/* <div id="accordion-collapse" data-accordion="collapse">
-							<h2 id="accordion-collapse-heading-1">
-								<button
-									type="button"
-									className="flex justify-between items-center p-5 w-full font-medium text-left text-gray-500 rounded-t-xl border border-b-0 border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800"
-									data-accordion-target="#accordion-collapse-body-1"
-									aria-expanded="true"
-									aria-controls="accordion-collapse-body-1"
-								>
-									<span>Bagaimana cara membeli produk thinker secara umum? </span>
-									<svg
-										data-accordion-icon
-										className="w-6 h-6 rotate-180 shrink-0"
-										fill="currentColor"
-										viewBox="0 0 20 20"
-										xmlns="http://www.w3.org/2000/svg"
-									>
-										<path
-											fill-rule="evenodd"
-											d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-											clip-rule="evenodd"
-										></path>
-									</svg>
-								</button>
-							</h2>
-							<div
-								id="accordion-collapse-body-1"
-								className=""
-								aria-labelledby="accordion-collapse-heading-1"
-							>
-								<div className="p-5 border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900">
-									<p className="mb-2 text-gray-500 dark:text-gray-400">
-										Setelah memasuki website Thinker, maka Anda harus melakukan
-										login (bagi yang sudah memiliki akun Thinker) atau daftar
-										(bagi yang belum memiliki akun Thinker). Setelah itu, Anda
-										dapat menuju bagian produk dan memilih produk apa saja yang
-										akan dibeli. Sebelum melakukan pembayaran, pastikan email,
-										alamat yang dimasukkan, dan pemilihan metode pengiriman
-										benar. Untuk produk hardcopy akan kami kirim ke alamat
-										penerima, sedangkan untuk produk softcopy dan konfirmasi
-										konsultasi akan kami kirim melalui email. Selanjutnya
-										silahkan lakukan pembayaran dengan metode yang tersedia, dan
-										tunggu produk tersebut sampai.
-									</p>
-								</div>
-							</div>
-							<h2 id="accordion-collapse-heading-2">
-								<button
-									type="button"
-									className="flex justify-between items-center p-5 w-full font-medium text-left text-gray-500 border border-b-0 border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800"
-									data-accordion-target="#accordion-collapse-body-2"
-									aria-expanded="false"
-									aria-controls="accordion-collapse-body-2"
-								>
-									<span>Bagaimana cara membuat akun Thinker?</span>
-									<svg
-										data-accordion-icon
-										className="w-6 h-6 shrink-0"
-										fill="currentColor"
-										viewBox="0 0 20 20"
-										xmlns="http://www.w3.org/2000/svg"
-									>
-										<path
-											fill-rule="evenodd"
-											d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-											clip-rule="evenodd"
-										></path>
-									</svg>
-								</button>
-							</h2>
-							<div
-								id="accordion-collapse-body-2"
-								className="hidden"
-								aria-labelledby="accordion-collapse-heading-2"
-							>
-								<div className="p-5 border border-b-0 border-gray-200 dark:border-gray-700">
-									<p className="mb-2 text-gray-500 dark:text-gray-400">
-										Terdapat dua cara yang dapat dilakukan, yaitu dengan
-										mengklik tombol Daftar, lalu masukkan email dan password.
-										Atau Anda juga dapat masuk ke akun Thinker dengan
-										menggunakan akun google Anda.
-									</p>
-								</div>
-							</div>
-							<h2 id="accordion-collapse-heading-3">
-								<button
-									type="button"
-									className="flex justify-between items-center p-5 w-full font-medium text-left text-gray-500 border border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800"
-									data-accordion-target="#accordion-collapse-body-3"
-									aria-expanded="false"
-									aria-controls="accordion-collapse-body-3"
-								>
-									<span>Bagaimana jika ingin merubah alamat pengiriman?</span>
-									<svg
-										data-accordion-icon
-										className="w-6 h-6 shrink-0"
-										fill="currentColor"
-										viewBox="0 0 20 20"
-										xmlns="http://www.w3.org/2000/svg"
-									>
-										<path
-											fill-rule="evenodd"
-											d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-											clip-rule="evenodd"
-										></path>
-									</svg>
-								</button>
-							</h2>
-							<div
-								id="accordion-collapse-body-3"
-								className="hidden"
-								aria-labelledby="accordion-collapse-heading-3"
-							>
-								<div className="p-5 border border-t-0 border-gray-200 dark:border-gray-700">
-									<p className="mb-2 text-gray-500 dark:text-gray-400">
-										Sebelum melakukan pembayaran, Anda diminta untuk memeriksa
-										kembali hal-hal yang diperlukan, termasuk alamat pengiriman.
-										Jika Anda ingin mengubahnya, maka Anda dapat mengklik alamat
-										yang Anda masukkan tersebut, lalu akan mengarah ke halaman
-										konfirmasi alamat, selanjutnya klik Ubah (ini ubah/ubah
-										alamat ya ) dan masukkan alamat baru yang Anda inginkan
-									</p>
-								</div>
-							</div>
-							<h2 id="accordion-collapse-heading-3">
-								<button
-									type="button"
-									className="flex justify-between items-center p-5 w-full font-medium text-left text-gray-500 border border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800"
-									data-accordion-target="#accordion-collapse-body-3"
-									aria-expanded="false"
-									aria-controls="accordion-collapse-body-3"
-								>
-									<span>Selain paypal, apakah ada metode pembayaran lain?</span>
-									<svg
-										data-accordion-icon
-										className="w-6 h-6 shrink-0"
-										fill="currentColor"
-										viewBox="0 0 20 20"
-										xmlns="http://www.w3.org/2000/svg"
-									>
-										<path
-											fill-rule="evenodd"
-											d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-											clip-rule="evenodd"
-										></path>
-									</svg>
-								</button>
-							</h2>
-							<div
-								id="accordion-collapse-body-3"
-								className="hidden"
-								aria-labelledby="accordion-collapse-heading-3"
-							>
-								<div className="p-5 border border-t-0 border-gray-200 dark:border-gray-700">
-									<p className="mb-2 text-gray-500 dark:text-gray-400">
-										Untuk saat ini, metode pembayaran yang tersedia hanyalah
-										Paypal.
-									</p>
-								</div>
-							</div>
-							<h2 id="accordion-collapse-heading-3">
-								<button
-									type="button"
-									className="flex justify-between items-center p-5 w-full font-medium text-left text-gray-500 border border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800"
-									data-accordion-target="#accordion-collapse-body-3"
-									aria-expanded="false"
-									aria-controls="accordion-collapse-body-3"
-								>
-									<span>
-										Siapa yang harus saya hubungi jika mengalami kendala?{' '}
-									</span>
-									<svg
-										data-accordion-icon
-										className="w-6 h-6 shrink-0"
-										fill="currentColor"
-										viewBox="0 0 20 20"
-										xmlns="http://www.w3.org/2000/svg"
-									>
-										<path
-											fill-rule="evenodd"
-											d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-											clip-rule="evenodd"
-										></path>
-									</svg>
-								</button>
-							</h2>
-							<div
-								id="accordion-collapse-body-3"
-								className="hidden"
-								aria-labelledby="accordion-collapse-heading-3"
-							>
-								<div className="p-5 border border-t-0 border-gray-200 dark:border-gray-700">
-									<p className="mb-2 text-gray-500 dark:text-gray-400">
-										Anda bisa menghubungi customer service Thinker melalui
-										WhatsApp kami, yaitu 082146751899 atau kontak kami melalui
-										sosial media @Thinker dan email ke thinker@gmail.com selama
-										jam operasionl berikut: Senin-Jumat, Pukul 07.30 - 08.00 WIB
-										Sabtu-Minggu, Pukul 09.00-17.00 WIB. Dalam mengirimkan
-										pengaduan, harap mengikuti format di bawah ini: Subjek:
-										Berisi topik permasalahan Nama Anda Ceritakan masalah yang
-										dialami
-									</p>
-								</div>
-							</div>
-						</div> */}
-						{/* <div className="col">
-							<h2>
-								Open <b>one</b>
-							</h2>
-							<div className="tabs">
-								<div className="tab">
-									<input type="radio" id="rd1" name="rd" />
-									<label className="tab-label" htmlFor="rd1">
-										Item 1
-									</label>
-									<div className="tab-content">
-										Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-										Eos, facilis.
-									</div>
-								</div>
-								<div className="tab">
-									<input type="radio" id="rd2" name="rd" />
-									<label className="tab-label" htmlFor="rd2">
-										Item 2
-									</label>
-									<div className="tab-content">
-										Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-										Nihil, aut.
-									</div>
-								</div>
-								<div className="tab">
-									<input type="radio" id="rd3" name="rd" />
-									<label htmlFor="rd3" className="tab-close">
-										Close others &times;
-									</label>
-								</div>
-							</div>
-						</div> */}
+						<div className="space-y-4 leading-8 pt-4">
+							{items.map((item, index) => (
+								<AccordionItem
+									key={index}
+									index={index}
+									data={item}
+									open={open}
+									setOpen={setOpen}
+								/>
+							))}
+						</div>
 					</div>
 
 					<img
@@ -303,6 +78,42 @@ const About = (): JSX.Element => {
 					/>
 				</section>
 			</MainLayout>
+		</div>
+	);
+};
+
+interface AccordionItemProps {
+	data: any;
+	setOpen(number): void;
+	index: number;
+	open: number;
+}
+
+const AccordionItem = ({ data, setOpen, open, index }: AccordionItemProps) => {
+	return (
+		<div className="pt-4">
+			<div className="flex text-lg cursor-pointer" onClick={() => setOpen(index === open ? 10 : index)}>
+				<div
+					className={`transition-all ${
+						index === open ? 'font-bold' : 'font-semibold'
+					}`}
+				>
+					{data.question}
+				</div>
+				{index === open ? (
+					<FiMinusCircle size={24} className="ml-auto" />
+				) : (
+					<FiPlusCircle size={24} className="ml-auto" />
+				)}
+			</div>
+
+			<div
+				className={`overflow-hidden transition-all duration-700 mr-4 ${
+					open === index ? 'max-h-120' : 'max-h-0'
+				}`}
+			>
+				{data.answer}
+			</div>
 		</div>
 	);
 };
